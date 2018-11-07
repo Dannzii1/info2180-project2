5d8648bfdc0ff29453c299c4228facbc52393000
dannzii <34076867+dannzii@users.noreply.github.com>
2018-11-06 20:32:08 -0500

    It till dont change the way the page looks

--- fifteen.js
+++ fifteen.js
@@ -1,5 +1,14 @@
 window.onload = main;
 
+
+var blank = ["300px", "300px"];
+var starting_time = 0;
+var timer;
+var total_time = 0;
+var best_time = 0;
+var best_moves = 0;
+
+
 function Starting() {
     var puzzleplace = document.getElementById("puzzlearea").childNodes;
     var starter = [];
@@ -33,4 +42,46 @@ function Starting() {
 
 function movable(pieces) {
     return parseInt(pieces.style.t) + 100 === parseInt(blank[0]) & parseInt(pieces.style.r) === parseInt(blank[1]) | parseInt(pieces.style.t) - 100 === parseInt(blank[0]) & parseInt(pieces.style.r) === parseInt(blank[1]) | parseInt(pieces.style.t) === parseInt(blank[0]) & parseInt(pieces.style.r) - 100 === parseInt(blank[1]) | parseInt(pieces.style.t) === parseInt(blank[0]) & parseInt(pieces.style.r) + 100 === parseInt(blank[1])
+}
+
+
+function wincheck(winning, piece) {
+    if (start) {
+        for (var i = 0; i < pieces.length; i++) {
+            if ((winning[i][0] !== piece[i].style.t) | (winning[i][1] !== piece[i].style.r)) {
+                return false;
+            }
+        }
+        clearInterval(timer);
+        return true;
+    }
+    return false;
+}
+
+function move_piece(piece, animate) {
+    blankTop = piece.style.t;
+    blankLeft = piece.style.r;
+
+    if (animate) {
+        var winning_state = arguments[2];
+        var pieces = arguments[3];
+        $(piece).animate({ "top": blank[0], "left": blank[1] }, "slow", "linear", function() {
+            if (check_for_win(winning_state, pieces)) {
+                if (best_time < total_time) {
+                    best_time = total_time;
+                }
+                if (best_moves < moves) {
+                    best_moves = moves
+                }
+                var winningstring = `You Win\nTotal Time: ${seconds_to_time(total_time)} Number of moves: ${moves}\nBest Time: ${seconds_to_time(best_time)} Best Number of Moves: ${best_moves}`;
+                $(".explanation")[0].innerText = winningstring;
+                $(".explanation")[0].style.textAlign = "Center";
+            }
+        });
+
+    } else {
+        piece.style.t = blank[0];
+        piece.style.r = blank[1];
+    }
+    blank = [blankTop, blankLeft];
 }
