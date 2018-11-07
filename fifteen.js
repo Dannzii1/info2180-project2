fc9eeca589a14422e5dee6d39b7c2f47d21f4c5f
dannzii <34076867+dannzii@users.noreply.github.com>
2018-11-07 14:39:27 -0500

    SO PEOPLE IT WORKS!!!!

--- fifteen.js
+++ fifteen.js
@@ -2,55 +2,56 @@ window.onload = main;
 
 
 var blank = ["300px", "300px"];
-var start = false
-var starting_time = 0;
+var start = false;
+var moves = 0;
+var startTime = 0;
 var timer;
 var total_time = 0;
 var best_time = 0;
 var best_moves = 0;
-var moves = 0;
 
 
 function starting() {
-    var puzzleplace = document.getElementById("puzzlearea").childNodes;
-    var starter = [];
+    var puzzleArea = document.getElementById("puzzlearea").childNodes;
+    var initial_state = [];
 
-    var a = 0,
-        x = 0,
-        t = 0,
-        r = 0,
-        counter = 1;
+    var x = 0,
+        y = 0,
+        top = 0,
+        left = 0,
+        piece_counter = 1;
 
-    for (let i = 0; i <puzzleplace.length; i++) {
-        if (puzzleplace[i].nodeName == "DIV") {
-            starter.push([t.toString() + "px", r.toString() + "px"]);
-           puzzleplace[i].className += "puzzlepiece";
-           puzzleplace[i].setAttribute("style", `background-position: ${a}px ${x}px; top: ${t}px; left: ${r}px;`);
-            a -= 100;
-            r += 100;
+    for (let i = 0; i < puzzleArea.length; i++) {
+        if (puzzleArea[i].nodeName == "DIV") {
+            initial_state.push([top.toString() + "px", left.toString() + "px"]);
+            puzzleArea[i].className += "puzzlepiece";
+            puzzleArea[i].setAttribute("style", `background-position: ${x}px ${y}px; top: ${top}px; left: ${left}px;`);
+            x -= 100;
+            left += 100;
 
-            if (counter % 4 == 0) {
-                x -= 100;
-                t += 100;
-                r = 0
+            if (piece_counter % 4 == 0) {
+                y -= 100;
+                top += 100;
+                left = 0
             }
-            counter += 1;
+            piece_counter += 1;
 
         }
     }
 
-    return starter
-}
-
-function movable(pieces) {
-    return parseInt(pieces.style.t) + 100 === parseInt(blank[0]) & parseInt(pieces.style.r) === parseInt(blank[1]) | parseInt(pieces.style.t) - 100 === parseInt(blank[0]) & parseInt(pieces.style.r) === parseInt(blank[1]) | parseInt(pieces.style.t) === parseInt(blank[0]) & parseInt(pieces.style.r) - 100 === parseInt(blank[1]) | parseInt(pieces.style.t) === parseInt(blank[0]) & parseInt(pieces.style.r) + 100 === parseInt(blank[1])
+    return initial_state
 }
 
 
-function wincheck(winning, piece) {
+function movable(piece) {
+    return parseInt(piece.style.top) + 100 === parseInt(blank[0]) & parseInt(piece.style.left) === parseInt(blank[1]) | parseInt(piece.style.top) - 100 === parseInt(blank[0]) & parseInt(piece.style.left) === parseInt(blank[1]) | parseInt(piece.style.top) === parseInt(blank[0]) & parseInt(piece.style.left) - 100 === parseInt(blank[1]) | parseInt(piece.style.top) === parseInt(blank[0]) & parseInt(piece.style.left) + 100 === parseInt(blank[1])
+}
+
+
+function winCheck(winningState, pieces) {
     if (start) {
         for (var i = 0; i < pieces.length; i++) {
-            if ((winning[i][0] !== piece[i].style.t) | (winning[i][1] !== piece[i].style.r)) {
+            if ((winningState[i][0] !== pieces[i].style.top) | (winningState[i][1] !== pieces[i].style.left)) {
                 return false;
             }
         }
@@ -60,44 +61,45 @@ function wincheck(winning, piece) {
     return false;
 }
 
-function movepiece(piece, animate) {
-    blankTop = piece.style.t;
-    blankLeft = piece.style.r;
+
+function movePiece(piece, animate) {
+    blank_top = piece.style.top;
+    blank_left = piece.style.left;
 
     if (animate) {
-        var winning_state = arguments[2];
+        var winningState = arguments[2];
         var pieces = arguments[3];
         $(piece).animate({ "top": blank[0], "left": blank[1] }, "slow", "linear", function() {
-            if (check_for_win(winning_state, pieces)) {
+            if (winCheck(winningState, pieces)) {
                 if (best_time < total_time) {
                     best_time = total_time;
                 }
                 if (best_moves < moves) {
                     best_moves = moves
                 }
-                var winningstring = `You Win\nTotal Time: ${seconds_to_time(total_time)} Number of moves: ${moves}\nBest Time: ${seconds_to_time(best_time)} Best Number of Moves: ${best_moves}`;
-                $(".explanation")[0].innerText = winningstring;
+                var win_string = `You Win\nTotal Time: ${time(total_time)} Number of moves: ${moves}\nBest Time: ${time(best_time)} Best Number of Moves: ${best_moves}`;
+                $(".explanation")[0].innerText = win_string;
                 $(".explanation")[0].style.textAlign = "Center";
             }
         });
 
     } else {
-        piece.style.t = blank[0];
-        piece.style.r = blank[1];
+        piece.style.top = blank[0];
+        piece.style.left = blank[1];
     }
-    blank = [blankTop, blankLeft];
+    blank = [blank_top, blank_left];
 }
 
 
-function random_shuffle(pieces) {
+function shuffle(pieces) {
     var pieceLength = pieces.length;
     var piece;
     var rand;
 
     for (var index = 0; index < pieceLength; index++) {
-        rand = Math.floor(Math.random() * pieceLength);
+        rand = Math.floor(Math.random() * pieces.length);
         piece = pieces.splice(rand, 1);
-        movepiece(piece[0], false);
+        movePiece(piece[0], false);
     }
 }
 
@@ -105,6 +107,7 @@ function random_shuffle(pieces) {
 function getPieces() {
     return $ (".puzzlepiece");
 }
+
 
 function time(seconds) {
     var date = new Date(null);
@@ -114,28 +117,62 @@ function time(seconds) {
 
 
 function updateTime() {
-    var currentDate = new Date();
-    var currentTime = (currentDate.getHours() * 60 * 60) + (currentDate.getMinutes() * 60) + currentDate.getSeconds();
-    totalTime = currentTime - starting_time;
-    return time(totalTime);
+    var current_date = new Date();
+    var current_time = (current_date.getHours() * 60 * 60) + (current_date.getMinutes() * 60) + current_date.getSeconds();
+    total_time = current_time - startTime;
+    return time(total_time);
 }
+
 
 function updateStatus() {
     $(".explanation")[0].innerHTML = `Time: ${updateTime()} Moves: ${moves}`;
 }
 
-function main() {
-    var winning_state = starting();
-    var puzzlePieces = getPieces();
+function backgroundSeletor() {
+    var background_form = "<form align='Center'>\
+    <p align='Center'>Welcome to my Game, His name is Hikari, Select shuffle and fix him! <p>\
+    </form>";
 
+    $("#overall").before(background_form);
+
+}
+
+function changeBackground(value) {
+    var pieces = getPieces();
+
+    for (var i = 0; i < pieces.length; i++){
+        pieces[i].style.backgroundImage = `url('background${value}.jpg')`;
+    }
+}
+
+function shuffleImage(){
+
+    var value = Math.floor(Math.random()*4)
+    if(value === 0){
+        value = "";
+    }
+}
+
+function main() {
+    var winningState = starting();
+    var puzzlePieces = getPieces();
+    backgroundSeletor();
+    var bg_form_items = $("form")[0].elements;
+
+    for (var i = 0; i < bg_form_items.length; i++) {
+        bg_form_items[i].addEventListener("click", function(){
+            changeBackground(this.value)
+        });
+    }
 
     document.getElementById("shufflebutton").onclick = function() {
-        randomShuffle(puzzlePieces);
+        shuffle(puzzlePieces);
+        shuffleImage();
         start = true;
         moves = 0;
         puzzlePieces = getPieces();
-        var startDate = new Date();
-        starting_time = (startDate.getHours() * 60 * 60) + (startDate.getMinutes() * 60) + startDate.getSeconds();
+        var start_date = new Date();
+        startTime = (start_date.getHours() * 60 * 60) + (start_date.getMinutes() * 60) + start_date.getSeconds();
         timer = setInterval(updateStatus, 1000);
     }
 
@@ -152,7 +189,7 @@ function main() {
 
         puzzlePieces[i].addEventListener("click", function() {
             if (this.className.includes("movablepiece")) {
-                movepiece(this, true, winning_state, puzzlePieces);
+                movePiece(this, true, winningState, puzzlePieces);
                 moves++;
             }
         });
